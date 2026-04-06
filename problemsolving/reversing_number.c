5a80b7163b44c7f892a6288dee80df3c81ebd7e4
MR.TEJ117 <mrtej117@gmail.com>
2026-04-06 22:32:59 +0530

    Add program to reverse a given number

---
+++ problemsolving/reversing_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    
+    int rev = 0,num,rem;
+    scanf("%d",&num);
+    while(num > 0){
+        rem = num % 10;
+        rev  = rev * 10 + rem;
+        num = num / 10;
+    }
+    printf("%d\n",rev);
+    return 0;
+}
+
